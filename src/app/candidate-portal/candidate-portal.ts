0c712b9f5bfea4fe151a9f912f88096b36938651
Shivani2161999 <shivanidakulga99@gmail.com>
2026-03-13 17:59:53 +0530

    Initial commit: Set up Angular with Adnate branding and Cordys login

---
+++ src/app/candidate-portal/candidate-portal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-candidate-portal',
+  imports: [],
+  templateUrl: './candidate-portal.html',
+  styleUrl: './candidate-portal.scss',
+})
+export class CandidatePortal {}
---
+++ src/app/interviewer-portal/interviewer-portal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interviewer-portal',
+  imports: [],
+  templateUrl: './interviewer-portal.html',
+  styleUrl: './interviewer-portal.scss',
+})
+export class InterviewerPortal {}
